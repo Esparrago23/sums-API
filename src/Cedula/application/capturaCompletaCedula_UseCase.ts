6d7453e036f349a186900eb0566a6104e2a2125b
AlejandroZBarajas <falejandrozb@gmail.com>
2026-07-24 09:24:31 -0600

    relaciones en caso de uso crear_cedula

--- src/Cedula/application/capturaCompletaCedula_UseCase.ts
+++ src/Cedula/application/capturaCompletaCedula_UseCase.ts
@@ -125,11 +125,23 @@ export class CapturaCompletaCedulaUseCase {
 
   private async createVivienda(nucleoId: number, direccionId: number | null, vivienda: Dict): Promise<number | null> {
     const materialTechoId = this.intValue(vivienda.material_techo_id)
-      ?? await this.findOrCreateCatalog('cat_material_muro_techo', 'id_material_muro_techo', 'nombre', vivienda.techo);
+      ?? await this.findOrCreateCatalog(
+        'cat_material', // 'cat_material_muro_techo',
+        'id_material', // 'id_material_muro_techo'
+        'nombre', vivienda.techo
+      );
     const materialParedesId = this.intValue(vivienda.material_paredes_id)
-      ?? await this.findOrCreateCatalog('cat_material_muro_techo', 'id_material_muro_techo', 'nombre', vivienda.paredes);
+      ?? await this.findOrCreateCatalog(
+        'cat_material', //'cat_material_muro_techo',
+        'id_material', // 'id_material_muro_techo',
+        'nombre', vivienda.paredes
+      );
     const materialPisoId = this.intValue(vivienda.material_piso_id)
-      ?? await this.findOrCreateCatalog('cat_material_piso', 'id_material_piso', 'nombre', vivienda.piso);
+      ?? await this.findOrCreateCatalog(
+        'cat_material', //      'cat_material_piso', 
+        'id_material', //  'id_material_piso', 
+        'nombre', vivienda.piso
+      );
     const manejoExcretasId = this.intValue(vivienda.manejo_excretas_id)
       ?? await this.findOrCreateCatalog('cat_manejo_excretas', 'id_manejo_excretas', 'nombre', vivienda.excretas);
     const cocinaUbicacionId = await this.findOrCreateCatalog('cat_ubicacion_cocina', 'id_ubicacion_cocina', 'nombre', vivienda.cocina_ubicacion ?? vivienda.cocina);
